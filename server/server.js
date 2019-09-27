5eb5e84e77c8d382709584174ced2f5454480860
Scott Torres <scott.torres27@gmail.com>
2019-09-27 09:38:46 -0700

    Implement getNewWord route to get one random word from API

--- server/server.js
+++ server/server.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const axios = require('axios');
 
 const PORT = 3000;
 const app = express();
 
 app.use(express.static('client/public'));
+
+app.get('/getNewWord', (req, res) => {
+  const randomInt = Math.floor(Math.random() * 162414);
+  axios
+    .get(`http://app.linkedin-reach.io/words?start=${randomInt}&count=1`)
+    .then(({ data }) => {
+      res.status(200).send(data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 app.listen(PORT, () => console.log(`Express server listening on PORT ${PORT}`));
