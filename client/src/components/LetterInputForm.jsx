25e9b722ded60a35a8996581e0a2eff116fe45e9
Scott Torres <scott.torres27@gmail.com>
2019-09-28 14:50:25 -0700

    Implement handleChange method for input field

--- client/src/components/LetterInputForm.jsx
+++ client/src/components/LetterInputForm.jsx
@@ -3,12 +3,19 @@ import { TextField, Button } from '@material-ui/core';
 
 const LetterInputForm = props => {
   const [letterInput, setLetterInput] = useState('');
+
+  const handleChange = e => {
+    setLetterInput(e.target.value);
+  };
+
   return (
     <form>
       <TextField
         id="letter-input"
         label="Guess A Letter:"
         value={letterInput.name}
+        onChange={handleChange}
+        onBlur={handleChange}
       />
     </form>
   );
