54283a0e33a946bca7f75fd2dbaae55619d2d456
Scott Torres <scott.torres27@gmail.com>
2019-09-30 11:22:17 -0700

    Set up get route to handle user making a guess

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 
 const PORT = 3000;
 const app = express();
+let secretWord = '';
 
 app.use(express.static('client/public'));
 app.use(morgan('tiny'));
@@ -14,12 +15,19 @@ app.get('/getInitialString', (req, res) => {
     .get(`http://app.linkedin-reach.io/words?start=${randomInt}&count=1`)
     .then(({ data }) => {
       const charactersArray = new Array(data.length);
+      secretWord = data;
       charactersArray.fill('_');
       res.status(200).send({ charactersArray, data });
     })
     .catch(err => {
       res.status(500).send({ Error: err });
     });
+});
+
+app.get('/checkGuess/:letter', (req, res) => {
+  const guessedLetter = req.params.letter;
+  console.log(secretWord);
+  res.status(200).send({ guessedLetter });
 });
 
 app.listen(PORT, () => console.log(`Express server listening on PORT ${PORT}`));
