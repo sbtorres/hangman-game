67df337c58910e78fbe0945a14dd145b907b6776
Scott Torres <scott.torres27@gmail.com>
2019-10-01 20:07:00 -0700

    Modal moved to center of screen

--- client/src/components/EndOfGameModal.jsx
+++ client/src/components/EndOfGameModal.jsx
@@ -4,6 +4,11 @@ import { Modal, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
   paper: {
     position: 'absolute',
     width: 400,
@@ -27,6 +32,7 @@ const EndOfGameModal = ({
 
   return (
     <Modal
+      className={classes.modal}
       aria-labelledby="simple-modal-title"
       aria-describedby="simple-modal-description"
       open={showEndOfGameModal}
