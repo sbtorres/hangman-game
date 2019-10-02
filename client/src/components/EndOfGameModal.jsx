16eb8a6f7c8330b68df9b2806f4388160d7cd4ee
Scott Torres <scott.torres27@gmail.com>
2019-10-01 20:09:41 -0700

    Center text within EndOfGameModal

--- client/src/components/EndOfGameModal.jsx
+++ client/src/components/EndOfGameModal.jsx
@@ -15,7 +15,8 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: theme.palette.background.paper,
     border: '2px solid #000',
     boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3)
+    padding: theme.spacing(2, 4, 3),
+    textAlign: 'center'
   }
 }));
 
