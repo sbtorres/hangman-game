c9e11e95e5165d68bc89a2344f19c21b1824af89
Scott Torres <scott.torres27@gmail.com>
2019-10-02 17:18:10 -0700

    Add 6 grids to cover LinkedIn logo

--- client/src/components/LinkedInImage.jsx
+++ client/src/components/LinkedInImage.jsx
@@ -3,12 +3,81 @@ import { Grid } from '@material-ui/core';
 
 const LinkedInImage = () => {
   return (
-    <Grid container justify="center" alignItems="center" alignContent="center">
+    <Grid
+      container
+      justify="center"
+      alignItems="center"
+      alignContent="center"
+      style={{ position: 'relative', height: '300px', width: '300px' }}
+    >
       <img
         src="images/linkedin.png"
         alt="linkedin"
-        style={{ height: '300px', width: '300px' }}
+        style={{
+          height: '300px',
+          width: '300px',
+          position: 'absolute',
+          zIndex: 2
+        }}
       />
+      <div style={{ display: 'flex' }}>
+        <Grid
+          style={{
+            height: '150px',
+            width: '100px',
+            position: 'relative',
+            backgroundColor: 'white',
+            zIndex: 3
+          }}
+        />
+        <Grid
+          style={{
+            height: '150px',
+            width: '100px',
+            position: 'relative',
+            backgroundColor: 'white',
+            zIndex: 3
+          }}
+        />
+        <Grid
+          style={{
+            height: '150px',
+            width: '100px',
+            position: 'relative',
+            backgroundColor: 'white',
+            zIndex: 3
+          }}
+        />
+      </div>
+      <div style={{ display: 'flex' }}>
+        <Grid
+          style={{
+            height: '150px',
+            width: '100px',
+            position: 'relative',
+            backgroundColor: 'white',
+            zIndex: 3
+          }}
+        />
+        <Grid
+          style={{
+            height: '150px',
+            width: '100px',
+            position: 'relative',
+            backgroundColor: 'white',
+            zIndex: 3
+          }}
+        />
+        <Grid
+          style={{
+            height: '150px',
+            width: '100px',
+            position: 'relative',
+            backgroundColor: 'white',
+            zIndex: 3
+          }}
+        />
+      </div>
     </Grid>
   );
 };
