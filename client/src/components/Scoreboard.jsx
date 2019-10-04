403dc733a728daab7685c4f22d58a22bfab2a27e
Scott Torres <scott.torres27@gmail.com>
2019-10-04 15:14:30 -0700

    Add margin to bottom of scoreboard

--- client/src/components/Scoreboard.jsx
+++ client/src/components/Scoreboard.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { AppBar, Typography } from '@material-ui/core';
 
+const useScoreboardStyles = makeStyles({
+  appbar: {
+    marginBottom: '10px'
+  }
+});
+
 const Scoreboard = () => {
+  const classes = useScoreboardStyles();
   return (
-    <AppBar position="static">
+    <AppBar position="static" className={classes.appbar}>
       <Typography variant="h6">Scoreboard</Typography>
     </AppBar>
   );
